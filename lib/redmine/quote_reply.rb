570dfaa3f26c979bd311391e4fb70e88beca7f3b
Katsuya Hidaka <hidakatsuya@gmail.com>
2024-09-19 01:07:19 +0900

    Separate the implementation for building a quote into Redmine::QuoteReply

--- lib/redmine/quote_reply.rb
+++ lib/redmine/quote_reply.rb
@@ -37,5 +37,51 @@ module Redmine
         )
       end
     end
+
+    module Builder
+      def quote_issue(issue, partial_quote: nil)
+        user = issue.author
+
+        build_quote(
+          "#{ll(Setting.default_language, :text_user_wrote, user)}\n> ",
+          issue.description,
+          partial_quote
+        )
+      end
+
+      def quote_issue_journal(journal, indice:, partial_quote: nil)
+        user = journal.user
+
+        build_quote(
+          "#{ll(Setting.default_language, :text_user_wrote_in, {value: journal.user, link: "#note-#{indice}"})}\n> ",
+          journal.notes,
+          partial_quote
+        )
+      end
+
+      def quote_root_message(message, partial_quote: nil)
+        build_quote(
+          "#{ll(Setting.default_language, :text_user_wrote, message.author)}\n> ",
+          message.content,
+          partial_quote
+        )
+      end
+
+      def quote_message(message, partial_quote: nil)
+        build_quote(
+          "#{ll(Setting.default_language, :text_user_wrote_in, {value: message.author, link: "message##{message.id}"})}\n> ",
+          message.content,
+          partial_quote
+        )
+      end
+
+      private
+
+      def build_quote(quote_header, text, partial_quote = nil)
+        quote_text = partial_quote.presence || text.to_s.strip.gsub(%r{<pre>(.*?)</pre>}m, '[...]')
+
+        "#{quote_header}#{quote_text.gsub(/(\r?\n|\r\n?)/, "\n> ") + "\n\n"}"
+      end
+    end
   end
 end
