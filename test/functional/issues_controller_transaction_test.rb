aced7da987f1c4205c9056652b7e0ede3abd0c27
Katsuya Hidaka <hidakatsuya@gmail.com>
2024-12-16 01:17:59 +0900

    Fix "cannot load such file -- issues_controller"

--- test/functional/issues_controller_transaction_test.rb
+++ test/functional/issues_controller_transaction_test.rb
@@ -18,10 +18,9 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 require_relative '../test_helper'
-require 'issues_controller'
 
 class IssuesControllerTransactionTest < Redmine::ControllerTest
-  tests IssuesController
+  tests :issues
   self.use_transactional_tests = false
 
   def setup
