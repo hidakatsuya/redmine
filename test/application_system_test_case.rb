c22bbcad79dbc84fd3076bc6f3592ac1070e5b07
Katsuya Hidaka <hidakatsuya@gmail.com>
2025-03-24 00:23:55 +0900

    Revert "fix"
    This reverts commit 27d0ebf75ccc2c8c2a2a231d2c360432fe2f5687.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -68,13 +68,13 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   # using default browser locale which depend on system locale for "real" browsers drivers
   def log_user(login, password)
     visit '/my/page'
-    assert_current_path '/login', ignore_query: true
+    assert_equal '/login', current_path
     within('#login-form form') do
       fill_in 'username', :with => login
       fill_in 'password', :with => password
       find('input[name=login]').click
     end
-    assert_current_path '/my/page', ignore_query: true
+    assert_equal '/my/page', current_path
   end
 
   def wait_for_ajax
