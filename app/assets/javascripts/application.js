9e326a1763d7ff41976607a228f4890e6ebafe25
Katsuya HIDAKA <hidakatsuya@gmail.com>
2025-04-02 13:25:18 +0900

    reaction feature

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1158,8 +1158,8 @@ function setupWikiTableSortableHeader() {
   });
 }
 
-$(function () {
-  $("[title]:not(.no-tooltip)").tooltip({
+function setupTooltip(container) {
+  $(container || 'body').find("[title]:not(.no-tooltip)").tooltip({
     show: {
       delay: 400
     },
@@ -1168,7 +1168,12 @@ $(function () {
       at: "center top"
     }
   });
-});
+}
+function removeTooltip(container) {
+  $(container || 'body').find("[title]:not(.no-tooltip)").tooltip('destroy')
+}
+
+$(function () { setupTooltip() });
 
 function inlineAutoComplete(element) {
     'use strict';
