360e25a9ed1d280bb9cce4118bdd6c2a11cd837b
Katsuya HIDAKA <hidakatsuya@gmail.com>
2026-08-02 13:16:02 +0900

    Move Gantt row markup into views

--- app/helpers/gantts/chart_helper.rb
+++ app/helpers/gantts/chart_helper.rb
@@ -2,17 +2,13 @@
 
 module Gantts
   module ChartHelper
-    GANTT_ROW_PARTIALS = {
-      Redmine::Gantt::Project => 'gantts/chart/rows/project',
-      Redmine::Gantt::Version => 'gantts/chart/rows/version',
-      Redmine::Gantt::Issue => 'gantts/chart/rows/issue'
+    ROW_SUBJECT_CLASSES = {
+      :project => 'project-name',
+      :version => 'version-name',
+      :issue => 'issue-subject'
     }.freeze
 
     SELECTED_COLUMN_WIDTH = 96
-
-    def gantt_row_partial(row)
-      GANTT_ROW_PARTIALS.fetch(row.class)
-    end
 
     def gantt_chart_tag(chart, project: nil, &)
       selected_columns_width = chart.selected_columns.size * SELECTED_COLUMN_WIDTH
@@ -41,22 +37,16 @@ module Gantts
         "--gantt-selected-columns-template: #{chart.selected_columns.map { "#{SELECTED_COLUMN_WIDTH}px" }.join(' ')}",
         "--gantt-subject-width: #{chart.sidebar_subject_width}px", "--gantt-timeline-width: #{chart.timeline_width}px"
       ].join('; ')
-      tag.div(:class => ['gantt', ('is-showing-columns' if chart.show_selected_columns?)], :style => styles,
+      tag.div(:class => ['gantt', {'is-showing-columns': chart.show_selected_columns?}], :style => styles,
               :data => data_attributes.merge('gantt-project-id': project&.id), &)
     end
 
     def gantt_scale_segment_css_classes(segment)
-      classes = ['gantt__scale-segment', "gantt__scale-segment--#{segment.kind.to_s.tr('_', '-')}"]
-      classes << 'is-non-working-day' if segment.non_working_day
-      classes
-    end
-
-    def gantt_scale_segment_content(segment, gantt)
-      if segment.kind == :month
-        link_to(segment.label, gantt.params.merge(:year => segment.start_on.year, :month => segment.start_on.month), :title => segment.title)
-      else
-        content_tag(:span, segment.label)
-      end
+      [
+        'gantt__scale-segment',
+        "gantt__scale-segment--#{segment.kind.to_s.tr('_', '-')}",
+        {'is-non-working-day': segment.non_working_day}
+      ]
     end
 
     def gantt_scale_segment_style(segment)
@@ -94,45 +84,38 @@ module Gantts
       ["--gantt-start-unit: #{schedule.bar_start_offset}", "--gantt-end-unit: #{schedule.late_offset}"].join('; ')
     end
 
-    def gantt_project_row_subject_tag(row)
-      gantt_row_subject_tag(row, gantt_project_subject_content(row), gantt_project_subject_css_classes(row),
-                            ['gantt__subject--project', 'project-name'])
+    def gantt_row_subject_tag(row, &)
+      tag.div(
+        :id => row.row_key,
+        :class => [
+          'gantt__subject',
+          "gantt__subject--#{row.kind}",
+          ROW_SUBJECT_CLASSES.fetch(row.kind),
+          {'is-open': row.expandable?, hascontextmenu: row.context_menu?}
+        ],
+        &
+      )
     end
 
-    def gantt_version_row_subject_tag(row)
-      gantt_row_subject_tag(row, gantt_version_subject_content(row), gantt_version_subject_css_classes(row),
-                            ['gantt__subject--version', 'version-name'])
-    end
-
-    def gantt_issue_row_subject_tag(row)
-      gantt_row_subject_tag(row, gantt_issue_subject_content(row), gantt_issue_subject_css_classes(row),
-                            ['gantt__subject--issue', 'issue-subject', 'hascontextmenu'])
-    end
-
-    def gantt_row_subject_tag(row, content, css_classes, container_classes)
-      expander =
-        if row.expandable?
-          content_tag(:button, sprite_icon('angle-down', :rtl => true), :type => 'button',
-                      :class => 'gantt__expander icon icon-expanded', :aria => {:expanded => 'true'},
-                      :data => {:action => 'click->gantt--subjects#toggleRow'})
-        else
-          content_tag(:span, '', :class => 'gantt__expander-placeholder', :aria => {:hidden => 'true'})
-        end
-      classes = ['gantt__subject', *container_classes]
-      classes << 'is-open' if row.expandable?
-      content_tag(:div, :class => classes.join(' '), :id => row.row_key) do
-        expander + content_tag(:span, content, :class => ['gantt__subject-text', *css_classes].join(' '), :title => row.subject)
-      end
-    end
-
-    def gantt_row_column_content(row, column)
-      return ''.html_safe unless row.issue?
-
-      content_tag(:div, column_content(column, row.issue), :class => ['gantt__cell-value', column.css_classes].compact.join(' '))
+    def gantt_row_subject_text_classes(row)
+      [
+        'gantt__subject-text',
+        {
+          'project-overdue': row.project? && row.overdue?,
+          'version-behind-schedule': row.version? && row.behind_schedule?,
+          'version-overdue': row.version? && row.overdue?,
+          'version-closed': row.version? && row.closed?,
+          'issue-overdue': row.issue? && row.overdue?,
+          'issue-behind-schedule': row.issue? && row.behind_schedule?,
+          'issue-closed': row.issue? && row.closed?,
+          'behind-start-date': !row.project? && row.behind_start_date?,
+          'over-end-date': !row.project? && row.over_end_date?
+        }
+      ]
     end
 
     def gantt_bar_classes(row)
-      [*gantt_bar_base_classes(row), 'gantt__bar', 'task_todo']
+      [*gantt_bar_base_classes(row), 'gantt__bar', 'task_todo', {hascontextmenu: row.context_menu?}]
     end
 
     def gantt_done_bar_classes(row)
@@ -162,60 +145,14 @@ module Gantts
 
     private
 
-    def gantt_project_subject_css_classes(row)
-      row.overdue? ? ['project-overdue'] : []
-    end
-
-    def gantt_version_subject_css_classes(row)
-      classes = []
-      classes << 'version-behind-schedule' if row.behind_schedule?
-      classes << 'version-overdue' if row.overdue?
-      classes << 'version-closed' if row.closed?
-      classes << 'behind-start-date' if row.behind_start_date?
-      classes << 'over-end-date' if row.over_end_date?
-      classes
-    end
-
-    def gantt_issue_subject_css_classes(row)
-      classes = []
-      classes << 'issue-overdue' if row.overdue?
-      classes << 'issue-behind-schedule' if row.behind_schedule?
-      classes << 'issue-closed' if row.closed?
-      classes << 'behind-start-date' if row.behind_start_date?
-      classes << 'over-end-date' if row.over_end_date?
-      classes
-    end
-
     def gantt_bar_base_classes(row)
-      classes = ['task', row.kind.to_s]
-      return classes unless row.issue?
-
-      classes.tap {|values| values[-1] = row.summary? ? 'parent' : 'leaf'}
-    end
-
-    def gantt_issue_subject_content(row)
-      issue = row.issue
-      content = +''
-      content << sprite_icon('issue') unless issue.assigned_to
-      content << assignee_avatar(issue.assigned_to, :size => 13, :class => 'icon-avatar')
-      content << link_to_issue(issue)
-      content << content_tag(:input, nil, :type => 'checkbox', :name => 'ids[]', :value => issue.id,
-                             :style => 'display:none;', :class => 'toggle-selection')
-      content.html_safe
-    end
-
-    def gantt_version_subject_content(row)
-      content = +''
-      content << sprite_icon('package')
-      content << link_to_version(row.version)
-      content.html_safe
-    end
-
-    def gantt_project_subject_content(row)
-      content = +''
-      content << sprite_icon('projects')
-      content << link_to_project(row.project)
-      content.html_safe
+      kind =
+        if row.issue?
+          row.summary? ? 'parent' : 'leaf'
+        else
+          row.kind.to_s
+        end
+      ['task', kind]
     end
   end
 end
