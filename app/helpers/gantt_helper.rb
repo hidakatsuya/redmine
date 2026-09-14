06798a722182bb3a1c357b885bccaa9dd5dc838b
Katsuya HIDAKA <hidakatsuya@gmail.com>
2026-09-15 08:57:42 +0900

    wip

--- app/helpers/gantt_helper.rb
+++ app/helpers/gantt_helper.rb
@@ -18,6 +18,150 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 module GanttHelper
+  class GanttChartLayout
+    Period = Struct.new(:date, :start, :width, :top, :height, :non_working, keyword_init: true)
+
+    attr_reader :chart_width, :content_height, :header_height, :headers_height, :zoom
+
+    def initialize(gantt)
+      @gantt = gantt
+      @zoom = 2**gantt.zoom
+      @header_height = 18
+      @headers_height = header_rows * header_height
+      @chart_width = ((gantt.date_to - gantt.date_from + 1) * zoom).to_i
+
+      gantt.render(top: 8, zoom: zoom, g_width: chart_width, subject_width: 330)
+      @content_height = [(20 * (gantt.number_of_rows + 6)) + 150, 206].max
+    end
+
+    def css_variables
+      {
+        'gantt-subject-width': '331px',
+        'gantt-header-height': "#{header_height}px",
+        'gantt-headers-height': "#{headers_height}px",
+        'gantt-chart-width': "#{chart_width - 1}px",
+        'gantt-content-height': "#{content_height}px",
+        'gantt-total-height': "#{headers_height + content_height + 24}px"
+      }
+    end
+
+    def months
+      date = @gantt.date_from
+      start = 0
+
+      Array.new(@gantt.months) do
+        width = (((date >> 1) - date) * zoom - 1).to_i
+        period = Period.new(
+          date: date,
+          start: start,
+          width: width,
+          top: 0,
+          height: show_weeks? ? header_height : header_height + content_height
+        )
+        start += width + 1
+        date >>= 1
+        period
+      end
+    end
+
+    def weeks
+      return [] unless show_weeks?
+
+      date = @gantt.date_from
+      start = 0
+      periods = []
+      unless date.cwday == 1
+        width = (7 - date.cwday + 1) * zoom - 1
+        periods << period(nil, start, width, header_height, weeks_height)
+        start += width + 1
+        date += 7 - date.cwday + 1
+      end
+
+      while date <= @gantt.date_to
+        width = ((date + 6 <= @gantt.date_to) ? 7 * zoom - 1 : (@gantt.date_to - date + 1) * zoom - 1).to_i
+        periods << period(date, start, width, header_height, weeks_height)
+        start += width + 1
+        date += 7
+      end
+      periods
+    end
+
+    def day_numbers
+      return [] unless show_day_numbers?
+
+      day_periods(header_height * 2, content_height + header_height * 2 - 1)
+    end
+
+    def days
+      return [] unless show_days?
+
+      day_periods(show_day_numbers? ? header_height * 3 : header_height * 2,
+                  content_height + header_height - 1)
+    end
+
+    def today_start
+      return unless User.current.today.between?(@gantt.date_from, @gantt.date_to)
+
+      (((User.current.today - @gantt.date_from + 1) * zoom).floor - 1).to_i
+    end
+
+    private
+
+    def header_rows
+      1 + (@gantt.zoom > 1 ? 1 : 0) + (@gantt.zoom > 2 ? 1 : 0) + (@gantt.zoom > 3 ? 1 : 0)
+    end
+
+    def show_weeks?
+      @gantt.zoom > 1
+    end
+
+    def show_days?
+      @gantt.zoom > 2
+    end
+
+    def show_day_numbers?
+      @gantt.zoom > 3
+    end
+
+    def weeks_height
+      show_days? ? header_height - 1 : header_height - 1 + content_height
+    end
+
+    def day_periods(top, height)
+      (@gantt.date_from..@gantt.date_to).each_with_index.map do |date, index|
+        Period.new(
+          date: date,
+          start: index * zoom,
+          width: zoom - 1,
+          top: top,
+          height: height,
+          non_working: @gantt.non_working_week_days.include?(date.cwday)
+        )
+      end
+    end
+
+    def period(date, start, width, top, height)
+      Period.new(date: date, start: start, width: width, top: top, height: height)
+    end
+  end
+
+  def gantt_chart_layout(gantt)
+    GanttChartLayout.new(gantt)
+  end
+
+  def gantt_css_variables(variables)
+    variables.map {|name, value| "--#{name}:#{value}"}.join(';')
+  end
+
+  def gantt_period_style(period)
+    gantt_css_variables(
+      'gantt-period-start': "#{period.start}px",
+      'gantt-period-width': "#{period.width}px",
+      'gantt-period-top': "#{period.top}px",
+      'gantt-period-height': "#{period.height}px"
+    )
+  end
+
   def gantt_zoom_link(gantt, in_or_out)
     case in_or_out
     when :in
@@ -42,7 +186,7 @@ module GanttHelper
     end
   end
 
-  def gantt_chart_tag(query, &)
+  def gantt_chart_tag(query, layout:, &)
     data_attributes = {
       controller: 'gantt--chart',
       # Events emitted by child controllers the chart listens to.
@@ -62,7 +206,12 @@ module GanttHelper
       'gantt--chart-show-progress-value': query.draw_progress_line ? 'true' : 'false'
     }
 
-    tag.table(class: 'gantt-table', data: data_attributes, &)
+    tag.div(
+      class: 'gantt-chart',
+      style: gantt_css_variables(layout.css_variables),
+      data: data_attributes,
+      &
+    )
   end
 
   def gantt_column_tag(column_name, min_width: nil, **options, &)
@@ -74,7 +223,9 @@ module GanttHelper
     }
     options[:class] = ["gantt_#{column_name}_column", options[:class]]
 
-    tag.td(**options, &)
+    options[:style] = gantt_css_variables('gantt-column-width': options.delete(:width)) if options[:width]
+
+    tag.div(**options, &)
   end
 
   def gantt_subjects_tag(&)
--- app/javascript/controllers/gantt/subjects_controller.js
+++ app/javascript/controllers/gantt/subjects_controller.js
@@ -33,8 +33,8 @@ export default class extends Controller {
       const $element = this.$(element)
       const json = $element.data("collapse-expand")
       const numberOfRows = $element.data("number-of-rows")
-      const barsSelector = `#gantt_area form > div[data-collapse-expand='${json.obj_id}'][data-number-of-rows='${numberOfRows}']`
-      const selectedColumnsSelector = `td.gantt_selected_column div[data-collapse-expand='${json.obj_id}'][data-number-of-rows='${numberOfRows}']`
+      const barsSelector = `#gantt_area .gantt-timeline-body form > .gantt_row[data-collapse-expand='${json.obj_id}'][data-number-of-rows='${numberOfRows}']`
+      const selectedColumnsSelector = `.gantt_selected_column .gantt_row[data-collapse-expand='${json.obj_id}'][data-number-of-rows='${numberOfRows}']`
 
       if (outOfHierarchy || this.#readInlineStart($element) <= subjectInlineStart) {
         outOfHierarchy = true
@@ -95,7 +95,9 @@ export default class extends Controller {
 
   #readInlineStart(el) {
     const node = el.jquery ? el[0] : el
-    return parseFloat(window.getComputedStyle(node).getPropertyValue("inset-inline-start"))
+    const style = window.getComputedStyle(node)
+    return parseFloat(style.getPropertyValue("inset-inline-start")) +
+      parseFloat(style.getPropertyValue("padding-inline-start"))
   }
 
   #readBlockStart(el) {
@@ -106,7 +108,7 @@ export default class extends Controller {
   #setBlockStart(el, value) {
     const node = el.jquery ? el[0] : el
     const px = typeof value === "number" ? `${value}px` : value
-    node.style.setProperty("inset-block-start", px)
+    node.style.setProperty("--gantt-row-top", px)
   }
 
   #setIconState(element, open) {
